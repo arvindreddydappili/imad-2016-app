3b8c6e7c78ff5472ea8027c631ae6466337d0504
arvindreddydappili <reddyarvind005@gmail.com>
2016-12-01 12:23:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,12 @@ img.onclick=function()
     var interval=setInterval(moveRight,10);
    
     };
+
+var setd = document.getElementById('date');
+
+setd.onclick=function(){
+  for(var i=0;i<=31;i++){
+      var d=document.getElementById('dd');
+      d.dd=i;
+  }  
+};
