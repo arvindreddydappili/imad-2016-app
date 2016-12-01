16c0444b1707a7b7e00028225bffd25d4617f9d2
arvindreddydappili <reddyarvind005@gmail.com>
2016-12-01 11:56:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element =document.getElementById('img');
-img.onclick=function()
+element.onclick=function()
 {
-    img.style.marginleft="100px";
+    element.style.marginleft="100px";
     };
