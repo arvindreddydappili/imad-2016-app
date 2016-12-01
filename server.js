86abcd8f5684e79911f25c5bc43d218876b8f70d
arvindreddydappili <reddyarvind005@gmail.com>
2016-12-01 08:08:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/ui/loginform.html',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','loginform.html'));
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
