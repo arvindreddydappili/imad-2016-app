5ccc731b3ddf5a1eeadf35ce27e6d3e99ca377e2
arvindreddydappili <reddyarvind005@gmail.com>
2016-12-01 08:45:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var pool = require('pg').pool; var config={
+    user : 'arvindreddydappili',
+    database : 'arvindreddydappili',
+    host : 'db.imad.hasura-app.io',
+    port :'5432',
+    password : process.env.DB_PASSKEY
+};
  var app = express();
 app.use(morgan('combined'));
 
-
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+            }
+            else{
+                res.send(JSON.stringfy(result));
+            }
+    });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
