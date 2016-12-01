8a4e27fb1eaf0546efed7ab1bd1dde0208c76e34
arvindreddydappili <reddyarvind005@gmail.com>
2016-12-01 12:08:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,15 @@
 console.log('Loaded!');
 var img = document.getElementById('madi');
+
+var marginLeft=0;
+
+function moveRight (){
+  marginLeft = marginLeft+100;
+  img.style.marginLeft= marginLeft+'px';
+}
+
 img.onclick=function()
 {
-    img.style.marginLeft = "100px";
+    var interval=setInterval(moveRight,100);
+   
     };
