a9ee123fa6629c2a6cb4899ac75a7d41ad7ab1f6
arvindreddydappili <reddyarvind005@gmail.com>
2016-12-06 07:17:33 +0530

    [imad-console] Updates ui/loginjs.js

--- ui/loginjs.js
+++ ui/loginjs.js
@@ -1,1 +1,34 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var Pool = require('pg').Pool; 
+var config ={
+    user : 'arvindreddydappili',
+    database : 'arvindreddydappili',
+    host : 'db.imad.hasura-app.io',
+    port :'5432',
+    password : process.env.DB_PASSWORD,
+};
+
+var app = express();
+app.use(morgan('combined'));
+
+var pool = new Pool(config);
+
+pool.query('INSERT INTO "login" (email,username, password,sex,dob) VALUES ($1, $2,$3,$4,$5)', [username, dbS], function (err,result){
+if (err) {
+res.status(500).send(err.toString());
+} else {
+res.send('user seccessfully registered' + username);
+}
+
+});
+
+
 alert(Welcome_to_login_page);
+
+
+app.get('/ui/loginjs.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'loginjs.js'));
+});
+
